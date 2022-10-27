e37b509ac26a5980fba5567af7cf309ff05f709c
Michael <mikeed1998@gmail.com>
2022-10-27 18:01:06 -0500

    cambios radicales a la busqueda

--- includes/includes.php
+++ includes/includes.php
@@ -354,5 +354,36 @@
 			});
 		</script>';
 
+		$scriptGNRL.='
+		<script>
+		$(document).ready(function(){
+			load_data();
+			function load_data(query)
+			{
+				$.ajax({
+					url:"fetch.php",
+					method:"post",
+					data:{query:query},
+					success:function(data)
+					{
+						$("#result").html(data);
+					}
+				});
+			}
+			
+			$("#search_text").keyup(function(){
+				var search = $(this).val();
+				if(search != "")
+				{
+					load_data(search);
+				}
+				else
+				{
+					load_data();			
+				}
+			});
+		});
+		</script>
+		';
 
 
--- pages/inicio.php
+++ pages/inicio.php
@@ -4,108 +4,164 @@
   
 <?=$header?>
 
-	<div class="bg-gris uk-text-center padding-bottom-50" style="min-height: 80vh;">
-		<div class="uk-container">
-			<div class="servicios-titulo padding-top-50">
-				<img src="img/design/APF_LOGO.png" alt="">
+<div class="bg-gris uk-text-center padding-bottom-50" style="min-height: 80vh;">
+	<div class="uk-container">
+		<div class="servicios-titulo padding-top-50">
+			<img src="img/design/APF_LOGO.png" alt="">
+		</div>
+		<div class="portafolio-subtitulo uk-flex uk-flex-center">
+			<div style="max-width: 700px;">
+				Hemos decidido crear esta página para brindar apoyo a los empresarios que se están viendo afectados en estos momentos
 			</div>
-			<div class="portafolio-subtitulo uk-flex uk-flex-center">
-				<div style="max-width: 700px;">
-					Hemos decidido crear esta página para brindar apoyo a los empresarios que se están viendo afectados en estos momentos
-				</div>
-			</div>
-			<div class="uk-flex uk-flex-center">
-				<div class="linea-roja"></div>
-			</div>
-			<div class="portafolio-contenedor">
-				<div uk-filter="target: .js-filter">
-					<!-- Cachando los servicos de la BD //-->
-				    <ul class="uk-subnav uk-subnav-pill uk-flex uk-flex-center">
-				        <li class="uk-active" uk-filter-control><a href="#">Todos</a></li>
-				        <?php 
-			    	    	$CONSULTA = $CONEXION -> query("SELECT * FROM servicios ORDER BY titulo");
-								while ($rowCONSULTA = $CONSULTA -> fetch_assoc()) {
-									$titulo = $rowCONSULTA['titulo'];
-						?>
-			        			<li uk-filter-control="[data-servicio='<?=$titulo?>']"><a href="#"><?=$titulo?></a></li>
-						<?php 
-								}
-		 				?>
-
+		</div>
+		<div class="uk-flex uk-flex-center">
+			<div class="linea-roja"></div>
+		</div>
+		<div class="portafolio-contenedor">
+			<div uk-filter="target: .js-filter">
+				<!-- Cachando los servicos de la BD //-->
+			    <ul class="uk-subnav uk-subnav-pill uk-flex uk-flex-center">
+			        <li class="uk-active" uk-filter-control>
+						<a href="#">
+							Todos
+						</a>
+					</li>
+			        <?php 
+		    	    	$CONSULTA = $CONEXION -> query("SELECT * FROM servicios ORDER BY titulo");
+							while ($rowCONSULTA = $CONSULTA -> fetch_assoc()) {
+								$titulo = $rowCONSULTA['titulo'];
+					?>
+		        				<li uk-filter-control="[data-servicio='<?=$titulo?>']">
+									<a href="#">
+										<?=$titulo?>
+									</a>
+								</li>
+					<?php 
+							}
+	 				?>
 			    </ul>
-				
+
+
+		
+
 				<!-- BARRA BUSQUEDA //-->
-				<div class="uk-container">
-					<div class="uk-padding">
-						<form class="uk-search uk-search-large" action="">
-							<a href="#" uk-search-icon></a>
-    						<input class="uk-search-input" type="search" placeholder="Search" aria-label="Search">
-						</form>
+				<div class="container">
+					<br><br><br>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon">Search</span>
+							<input type="text" name="search_text" id="search_text" placeholder="Search by Customer Details" class="form-control" />
+						</div>
 					</div>
+					<br>
+					<div id="result"></div>
 				</div>
-				
-			    <ul class="js-filter uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-text-center" uk-grid>
-				<?php 
-			        $CONSULTA = $CONEXION -> query("SELECT * FROM productos");
-					while ($rowCONSULTA = $CONSULTA -> fetch_assoc()) {
-						$id = $rowCONSULTA['id'];
-						$idEmpresa = $rowCONSULTA['empresa'];
-						$idServicio = $rowCONSULTA['servicio'];
-						$whatsapp = $rowCONSULTA['whatsapp'];
-						$urlTitulo = urlencode(str_replace($caracteres_no_validos,$caracteres_si_validos, html_entity_decode(strtolower($rowCONSULTA['titulo']))));
-						$link = $id.'_'.$urlTitulo.'-.html';
+				<div style="clear:both"></div>
 
-						$CONSULTASERV = $CONEXION -> query("SELECT * FROM servicios WHERE id = $idServicio");
-						$rowCONSULTASERV = $CONSULTASERV -> fetch_assoc();
-						$nombreServicio = $rowCONSULTASERV['titulo'];
+				<script>
+		$(document).ready(function(){
+			load_data();
+			function load_data(query)
+			{
+				$.ajax({
+					url:"pages/fetch.php",
+					method:"post",
+					data:{query:query},
+					success:function(data)
+					{
+						$("#result").html(data);
+					}
+				});
+			}
+			
+			$("#search_text").keyup(function(){
+				var search = $(this).val();
+				if(search != "")
+				{
+					load_data(search);
+				}
+				else
+				{
+					load_data();			
+				}
+			});
+		});
+		</script>
 
-						$imagen = $rowCONSULTA['imagen'];
 
-                    echo '
-                    	<li data-servicio="'.$nombreServicio.'" class="card-principal">
-                        	<div uk-grid class="uk-grid-collapse" style="padding-top: 80px">
-                          		<div class="uk-width-expand bg-white uk-text-center padding-top-20">
-            
-                          		</div>
-                          		<div class="uk-width-auto uk-position-relative">
-                              		<img src="img/design/testimonios-circulo.png" style="width: 150px;" alt="">
-                              		<div class="uk-position-top-center" style="top:-65px; background: white; border-radius: 50%; height: 124px; width: 124px; overflow: hidden;">
-                                		<img src="img/contenido/productos/'.$rowCONSULTA['imagen'].'" alt="" uk-cover>
-                              		</div>
-                          		</div>
-                            	<div class="uk-width-expand bg-white uk-text-center padding-top-20">
-							
-                          		</div>
-                        	</div>
-                        	<div class="bg-white testimonios-card">
-                          		<div class="testimonios-nombre">
-                            		'.$rowCONSULTA['titulo'].'
-                          		</div>
-                          	<div class="testimonios-ramo uk-text-uppercase">
-                            	'.$rowCONSULTA['empresa'].'
-                          	</div>
-                          	<div class="testimonios-texto">
-                          		'.wordlimit($rowCONSULTA['txt'] ,'20','...').'
-                          	</div>
-                          	<div uk-grid class="padding-bottom-10 padding-top-20">
-                            	<div class="uk-width-expand uk-text-left">
-                              		<a target="_blank" href="https://wa.me/521'.$whatsapp.'?text=Me%20gustaría%20saber%20..." class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:whatsapp; ratio: .8"></a> &nbsp;
-                              		<a target="_blank" href="'.$rowCONSULTA['facebook'].'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:facebook; ratio: .8"></a> &nbsp;
-                              		<a target="_blank" href="'.$rowCONSULTA['instagram'].'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:instagram; ratio: .8"></a>
-                            	</div>
-                            	<div class="uk-width-auto testimonios-proyectos uk-flex uk-flex-middle">
-                              		<a href="'.$link.'">
-                                		Ver más <span class="color-negro" uk-icon="icon: arrow-right; ratio: .8"></span>
-                              		</a>
-                            	</div>
-                          	</div>
-                        </div>
-                    </li>';
-                   	} 
-                 echo '
-                </ul>';
-			    ?>
-			    </ul>
+				<ul class="js-filter uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-text-center" uk-grid>
+					<?php 
+						$aux = true;
+
+						if($aux == true)
+						{
+							// $CONSULTA = $CONEXION -> query("SELECT * FROM productos WHERE txt LIKE '%mentos%'");
+							$CONSULTA = $CONEXION -> query("SELECT * FROM productos");
+						
+							while ($rowCONSULTA = $CONSULTA -> fetch_assoc()) {
+								$id = $rowCONSULTA['id'];
+								$idEmpresa = $rowCONSULTA['empresa'];
+								$idServicio = $rowCONSULTA['servicio'];
+								$whatsapp = $rowCONSULTA['whatsapp'];
+								$urlTitulo = urlencode(str_replace($caracteres_no_validos,$caracteres_si_validos, html_entity_decode(strtolower($rowCONSULTA['titulo']))));
+								$link = $id.'_'.$urlTitulo.'-.html';
+	
+								$CONSULTASERV = $CONEXION -> query("SELECT * FROM servicios WHERE id = $idServicio");
+								$rowCONSULTASERV = $CONSULTASERV -> fetch_assoc();
+								$nombreServicio = $rowCONSULTASERV['titulo'];
+	
+								$imagen = $rowCONSULTA['imagen'];
+	
+								echo '
+									<li data-servicio="'.$nombreServicio.'" class="card-principal">
+										<div uk-grid class="uk-grid-collapse" style="padding-top: 80px">
+											<div class="uk-width-expand bg-white uk-text-center padding-top-20">
+											  </div>
+											  <div class="uk-width-auto uk-position-relative">
+												  <img src="img/design/testimonios-circulo.png" style="width: 150px;" alt="">
+												  <div class="uk-position-top-center" style="top:-65px; background: white; border-radius: 50%; height: 124px; width: 124px; overflow: hidden;">
+													<img src="img/contenido/productos/'.$rowCONSULTA['imagen'].'" alt="" uk-cover>
+												  </div>
+											  </div>
+											<div class="uk-width-expand bg-white uk-text-center padding-top-20">
+											  </div>
+										</div>
+										<div class="bg-white testimonios-card">
+											  <div class="testimonios-nombre">
+												'.$rowCONSULTA['titulo'].'
+											  </div>
+											  <div class="testimonios-ramo uk-text-uppercase">
+												'.$rowCONSULTA['empresa'].'
+											  </div>
+											  <div class="testimonios-texto">
+												  '.wordlimit($rowCONSULTA['txt'] ,'20','...').'
+											</div>
+											  <div uk-grid class="padding-bottom-10 padding-top-20">
+												<div class="uk-width-expand uk-text-left">
+													  <a target="_blank" href="https://wa.me/521'.$whatsapp.'?text=Me%20gustaría%20saber%20..." class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:whatsapp; ratio: .8"></a> &nbsp;
+													  <a target="_blank" href="'.$rowCONSULTA['facebook'].'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:facebook; ratio: .8"></a> &nbsp;
+													  <a target="_blank" href="'.$rowCONSULTA['instagram'].'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:instagram; ratio: .8"></a>
+												</div>
+												<div class="uk-width-auto testimonios-proyectos uk-flex uk-flex-middle">
+													  <a href="'.$link.'">
+														Ver más <span class="color-negro" uk-icon="icon: arrow-right; ratio: .8"></span>
+													</a>
+												</div>
+											  </div>
+										</div>
+									</li>';
+							   } 
+								 
+							echo '
+								</ul>';
+						}
+						else
+						{
+							echo "Nada";
+						}
+			       		
+			    	?>
+				</ul>
 			</div>
 		</div>
 	</div>
