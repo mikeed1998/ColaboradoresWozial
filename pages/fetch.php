a7eb402672548077ce30e9cb9f7c18fb3642a9e6
Michael <mikeed1998@gmail.com>
2022-10-28 17:42:12 -0500

    funcionando, falta lo de vacios y acomodar el design

--- pages/fetch.php
+++ pages/fetch.php
@@ -38,6 +38,51 @@ while($result) {
 }
 */
 
+function mostarDatos($a, $b, $c, $d, $e, $f, $g, $h, $i) {
+	$output = '';
+	$output.='
+	 	<ul class="js-filter uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-text-center" uk-grid>
+	 		<li data-servicio="'.$a.'" class="card-principal">
+	 			<div uk-grid class="uk-grid-collapse" style="padding-top: 80px">
+	 				<div class="uk-width-expand bg-white uk-text-center padding-top-20">
+	 			  	</div>
+	 			  	<div class="uk-width-auto uk-position-relative">
+	 				  	<img src="img/design/testimonios-circulo.png" style="width: 150px;" alt="">
+	 				  	<div class="uk-position-top-center" style="top:-65px; background: white; border-radius: 50%; height: 124px; width: 124px; overflow: hidden;">
+	 						<img src="img/contenido/productos/'. $b .'" alt="" uk-cover>
+	 				  	</div>
+	 			  	</div>
+	 				<div class="uk-width-expand bg-white uk-text-center padding-top-20">
+	 			  	</div>
+	 			</div>
+	 			<div class="bg-white testimonios-card">
+	 			  	<div class="testimonios-nombre">
+	 					'.$c.'
+	 			  	</div>
+	 			  	<div class="testimonios-ramo uk-text-uppercase">
+	 					'.$d.'
+	 			  	</div>
+	 			  	<div class="testimonios-texto">
+	 			  		'.wordlimit($e ,'20','...').'
+	 				</div>
+	 			  	<div uk-grid class="padding-bottom-10 padding-top-20">
+	 					<div class="uk-width-expand uk-text-left">
+	 					  	<a target="_blank" href="https:wa.me/521'.$f.'?text=Me%20gustaría%20saber%20..." class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:whatsapp; ratio: .8"></a> &nbsp;
+	 					  	<a target="_blank" href="'.$g.'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:facebook; ratio: .8"></a> &nbsp;
+	 					  	<a target="_blank" href="'.$h.'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:instagram; ratio: .8"></a>
+	 					</div>
+	 					<div class="uk-width-auto testimonios-proyectos uk-flex uk-flex-middle">
+	 						<a href="'.$i.'">
+	 							Ver más <span class="color-negro" uk-icon="icon: arrow-right; ratio: .8"></span>
+	 						</a>
+	 					</div>
+	 			  	</div>
+	 			</div>
+	 		</li>
+		</ul>';
+	
+	echo $output;
+}
 
 if(isset($_POST["query"]))
 {
@@ -136,57 +181,13 @@ else
 		$link = "";
 
 	 	/* data-servicio=/*$nombreServicio*/
-		var_dump($rowCONSULTA['titulo']);
+		// var_dump($rowCONSULTA['titulo']);
 	
 
-		mostarDatos($nombreServicio, $rowCONSULTA['imagen'], $rowCONSULTA['titulo'], $rowCONSULTA['empresa'], $rowCONSULTA['txt'], $rowCONSULTA['facebook'], $rowCONSULTA['instagram'], $link);
-	/*
-	 	$output.='
-	 	<ul class="js-filter uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-text-center" uk-grid>
-	 		<li data-servicio="'.$nombreServicio.'" class="card-principal">
-	 			<div uk-grid class="uk-grid-collapse" style="padding-top: 80px">
-	 				<div class="uk-width-expand bg-white uk-text-center padding-top-20">
-	 			  	</div>
-	 			  	<div class="uk-width-auto uk-position-relative">
-	 				  	<img src="img/design/testimonios-circulo.png" style="width: 150px;" alt="">
-	 				  	<div class="uk-position-top-center" style="top:-65px; background: white; border-radius: 50%; height: 124px; width: 124px; overflow: hidden;">
-	 						<img src="img/contenido/productos/'. $rowCONSULTA['imagen'] .'" alt="" uk-cover>
-	 				  	</div>
-	 			  	</div>
-	 				<div class="uk-width-expand bg-white uk-text-center padding-top-20">
-	 			  	</div>
-	 			</div>
-	 			<div class="bg-white testimonios-card">
-	 			  	<div class="testimonios-nombre">
-	 					'.$rowCONSULTA['titulo'].'
-	 			  	</div>
-	 			  	<div class="testimonios-ramo uk-text-uppercase">
-	 					'.$rowCONSULTA['empresa'].'
-	 			  	</div>
-	 			  	<div class="testimonios-texto">
-	 			  		'.wordlimit($rowCONSULTA['txt'] ,'20','...').'
-	 				</div>
-	 			  	<div uk-grid class="padding-bottom-10 padding-top-20">
-	 					<div class="uk-width-expand uk-text-left">
-	 					  	<a target="_blank" href="https:wa.me/521'.$whatsapp.'?text=Me%20gustaría%20saber%20..." class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:whatsapp; ratio: .8"></a> &nbsp;
-	 					  	<a target="_blank" href="'.$rowCONSULTA['facebook'].'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:facebook; ratio: .8"></a> &nbsp;
-	 					  	<a target="_blank" href="'.$rowCONSULTA['instagram'].'" class="uk-icon-button" style="height: 20px; width: 20px;" uk-icon="icon:instagram; ratio: .8"></a>
-	 					</div>
-	 					<div class="uk-width-auto testimonios-proyectos uk-flex uk-flex-middle">
-	 						<a href="'.$link.'">
-	 							Ver más <span class="color-negro" uk-icon="icon: arrow-right; ratio: .8"></span>
-	 						</a>
-	 					</div>
-	 			  	</div>
-	 			</div>
-	 		</li>
-		</ul>';
-	*/
+		mostarDatos($nombreServicio, $rowCONSULTA['imagen'], $rowCONSULTA['titulo'], $rowCONSULTA['empresa'], $rowCONSULTA['txt'], $rowCONSULTA['whatsapp'], $rowCONSULTA['facebook'], $rowCONSULTA['instagram'], $link);
 	
-	var_dump($output);
-
 	//echo $output;
-}
+	}
 
 	
 
